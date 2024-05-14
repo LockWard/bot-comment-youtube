d2dc18e5b7125816628368009a0b16a3de9f5f15
LockWard <roger1210@live.com>
2024-05-13 23:35:59 -0400

    Dist folder deleted

--- dist/config/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GOOGLE_REDIRECT_URL = exports.GOOGLE_CLIENT_SECRET = exports.GOOGLE_CLIENT_ID = exports.PORT = void 0;
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-exports.PORT = process.env.PORT || 3000;
-exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-exports.GOOGLE_REDIRECT_URL = process.env.GOOGLE_REDIRECT_URL;
